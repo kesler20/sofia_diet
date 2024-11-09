e4cb50e18f2dad444f4989b6e7a8ddcb07d0fc41
Kesler <uchekesla@gmail.com>
2024-11-09 18:20:32 +0000

    reset

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./styles/index.css"
 
+
 export default function App() {
   return (
     <div className="min-h-screen w-full">
