4bbb4ff967d8de41a3ea9d0bf7c0570545dc046f
Kesler <uchekesla@gmail.com>
2025-03-09 04:45:38 +0000

    added authentication and CI-Cd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import NavbarComponent from "./components/navbar/NavbarComponent";
 import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/index.css"
+import "./styles/index.css";
+import Auth0ProviderWithHistory from "./Auth0Provider";
 
 export default function App() {
   return (
     <div className="min-h-screen w-full">
       <BrowserRouter>
-        <NavbarComponent />
-        <Pages />
+        <Auth0ProviderWithHistory>
+          <NavbarComponent />
+          <Pages />
+        </Auth0ProviderWithHistory>
       </BrowserRouter>
     </div>
   );
